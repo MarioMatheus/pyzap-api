e065ebcc14d43750ee85d8d96a98ec5e91e02fc1
MarioMatheus <mariomatheu@gmail.com>
2020-08-10 01:49:50 -0300

    added broker business

--- src/domain/broker.py
+++ src/domain/broker.py
@@ -1,17 +1,31 @@
 import stomp
 import sys
-import time
 
-class Listener(stomp.ConnectionListener):
-    def on_error(self, metadata, message):
-        print('received an error "%s"' % message)
-    def on_message(self, metadata, message):
-        print('received a message "%s"' % message)
+class BrokerConnection:
 
-conn = stomp.Connection()
-conn.set_listener('', Listener())
-conn.connect('admin', 'password', wait=True)
-conn.subscribe(destination='/queue/test', id=1, ack='auto')
-conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
-time.sleep(2)
-conn.disconnect()
+    def __init__(self):
+        self.conn = stomp.Connection()
+        self.conn.connect('admin', 'password', wait=True)
+
+    def __del__(self):
+        self.conn.disconnect()
+
+    def listen(self, on_message_recv):
+        class Listener(stomp.ConnectionListener):
+                    def on_error(self, headers, body):
+                        print('received an error "%s"' % body)
+                    def on_message(self, headers, body):
+                        print('received a message "%s"' % body)
+                        on_message_recv(body)
+        self.conn.set_listener('', Listener())
+
+    def send_message(self, queue, message):
+        self.conn.send(body=message, destination='/queue/' + queue)
+
+    def get_messages_from_queue(self, queue):
+        self.conn.subscribe(destination='/queue/' + queue, id=queue, ack='auto')
+
+    def stop_messages_from_queue(self, queue):
+        self.conn.unsubscribe(queue)
+
+
--- src/domain/service.py
+++ src/domain/service.py
@@ -1,4 +1,5 @@
 import threading
+from . import broker
 from model import ZapMessageDecode, ZapMessageEncode
 
 class ZapService:
@@ -6,20 +7,31 @@ class ZapService:
     def __init__(self):
         self.users = {}
         self.lock = threading.Lock()
+        self.broker = broker.BrokerConnection()
+        self.broker.listen(self.broker_on_message)
 
     def append_user(self, user):
         print('Adding user to service')
         self.lock.acquire()
         self.users[user.name] = user
         print(self.users.keys())
-        threading.Thread(target=self.thread_run, args=(user.name, user.connection), name='USER_CLIENT').start()
+        self.broker.get_messages_from_queue(user.name)
+        threading.Thread(target=self.thread_run, args=(user.name, user.connection), name='USER_CLIENT::' + user.name).start()
         self.lock.release()
+
+    def broker_on_message(self, message):
+        zap_message = ZapMessageDecode(message.encode())
+        self.send_message(zap_message)
 
     def thread_run(self, username, connection):
         print(username + ' on')
+        is_subscribed = True
         while True:
             try:
                 data = connection.recv(2048)
+                if is_subscribed:
+                    self.broker.stop_messages_from_queue(username)
+                    is_subscribed = False
                 if data:
                     message = ZapMessageDecode(data)
                     self.send_message(message)
@@ -34,7 +46,8 @@ class ZapService:
             receiver_user.connection.send(ZapMessageEncode(message))
             print(message.sender + ' sent message to ' + message.receiver)
         else:
-            print('[ACTIVEMQ] hora do broker')
+            self.broker.send_message(message.receiver, ZapMessageEncode(message))
+            print('message sent to broker')
 
     def remove(self, username):
         if username in self.users.keys():
